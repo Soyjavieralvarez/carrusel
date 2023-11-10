611f1a7860441a22b6057b5d7d2b9d814cd2c3da
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-11-10 19:38:06 +0100

    main.js created

---
+++ main.js
@@ -0,0 +1,4 @@
+const carouselV1 = (carouselType) => {
+    const slides$$ = document.querySelectorAll(`.slider__slide--${carouselType}`);
+    const image$$ = document.querySelectorAll(`.carouselV1__asset--${carouselType}`)
+}
