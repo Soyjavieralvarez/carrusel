6fb9d4945b5b2d996bf97d286103229c08c49797
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-11-10 20:10:09 +0100

    changes to html

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-const carouselV1 = (carouselType) => {
+/**
+ * 
+ * @param {string} carouselType 
+ */
+
+
+const carouselV2 = (carouselType) => {
     const slides$$ = document.querySelectorAll(`.slider__slide--${carouselType}`);
-    const image$$ = document.querySelectorAll(`.carouselV1__asset--${carouselType}`)
+    const images$$ = document.querySelectorAll(`.carouselV2__asset--${carouselType}`);
 }
+
+// Aplicamos el manejo de slides actualizando las clases
+
+slides$$.forEach((slide, index) => 
+slide.addEventListener("click", () => {
+    if (carouselType === "models") {
+        slides$$.forEach((slide) => slide.classList.remove("isSlideActive"));
+        image$$.forEach((image) => image.classList.add("inactive"));
+
+        if (index >= 0) {
+            slides$$[index].classList.add("isSlideActive");
+            images$$[index].classList.remove("inactive");
+        }
+    }
+})
+)
+
+carouselV2("models");
+
+buttonShowcase1$$.addEventListener("click", () => {
+});
+
+buttonShowcase2$$.addEventListener("click", () => {
+});
+
